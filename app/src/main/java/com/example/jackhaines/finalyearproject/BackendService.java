0085af0fb49d534591102ccbcb048ccd4f15b732
Jack Haines <jack.r.haines@outlook.com>
2019-01-30 00:09:02 +0000

    update backend service #2

--- app/src/main/java/com/example/jackhaines/finalyearproject/BackendService.java
+++ app/src/main/java/com/example/jackhaines/finalyearproject/BackendService.java
@@ -3,6 +3,15 @@ package com.example.jackhaines.finalyearproject;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class BackendService extends AsyncTask<String, Void, Void> {
 
     Context context;
@@ -14,8 +23,24 @@ public class BackendService extends AsyncTask<String, Void, Void> {
     @Override
     protected Void doInBackground(String... voids) {
         String type = voids[0];
+        String submit_url = "http://35.246.45.206/submit.php";
         if(type.equals("submit")){
-            
+            try {
+                URL url = new URL(submit_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("");
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         return null;
